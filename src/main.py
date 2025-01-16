8fa16d43442e06788c5c5bffb9c96428bc1b7184
NikNikolaenkov <niknikolaenkov@gmail.com>
2025-01-16 14:04:01 +0200

    Refactor Dockerfile and main.py for improved structure and clarity
    - Updated Dockerfile to copy all application files instead of just the src directory, streamlining the build process.
    - Modified import statements in main.py to use absolute imports, enhancing code readability and maintainability.

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, send_file, jsonify
 import os
-from .translator import PotTranslator
-from .config import Config
+from src.translator import PotTranslator
+from src.config import Config
 
 app = Flask(__name__)
 
